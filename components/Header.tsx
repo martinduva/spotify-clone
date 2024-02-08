e71bd57f514632679f775e1fc1bd3fbc99660dfc
Martin D'Uva <martinduva1998@hotmail.com>
2024-02-08 10:54:30 -0300

    feat(header): add sign up and log in buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 
+import Button from "@/components/Button";
 import cn from "@/lib/cn";
 
 type HeaderProps = {
@@ -40,6 +41,14 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
           <button className="flex items-center justify-center rounded-full bg-white p-2 transition hover:opacity-75">
             <BiSearch className="text-black" size={20} />
           </button>
+        </div>
+        <div className="flex items-center justify-between gap-x-4">
+          <Button className="text-nowrap bg-transparent font-medium text-neutral-300" onClick={() => {}}>
+            Sign up
+          </Button>
+          <Button className="text-nowrap bg-white px-6 py-2" onClick={() => {}}>
+            Log in
+          </Button>
         </div>
       </div>
     </div>
