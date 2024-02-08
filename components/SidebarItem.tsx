bc14cb6d7e34ac039b1a711ecbe1882b3be10d35
Martin D'Uva <martinduva1998@hotmail.com>
2024-02-07 23:26:33 -0300

    feat(sidebar): add home and search navigation items

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,5 +1,27 @@
-const SidebarItem = () => {
-  return <div>Sidebar Item</div>;
+import Link from "next/link";
+import { IconType } from "react-icons";
+import cn from "@/lib/cn";
+
+type SidebarItemProps = {
+  icon: IconType;
+  label: string;
+  active: boolean;
+  href: string;
+};
+
+const SidebarItem: React.FC<SidebarItemProps> = ({ icon: Icon, label, active, href }) => {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1",
+        { "text-white": active }
+      )}
+    >
+      <Icon size={26} />
+      <p className="w-full truncate">{label}</p>
+    </Link>
+  );
 };
 
 export default SidebarItem;
